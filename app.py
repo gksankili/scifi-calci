4043ade9fc024e276cb94284d070b8c406ff752b
gksankili <gksankili@gmail.com>
2025-06-15 17:39:40 +0530

    Updated code

--- app.py
+++ app.py
@@ -15,4 +15,4 @@ def index():
     return render_template('index.html', result=result)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5011)
+    app.run(debug=True, host='0.0.0.0', port=5011)
